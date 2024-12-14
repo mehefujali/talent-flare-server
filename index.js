a869342151ca267836d4498def34a9760c3a79a3
Mehefuj Ali <mehefujalim@gmail.com>
2024-12-14 12:51:03 +0530

    user api crated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const app = express() 
+const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 8080
 //mw 
@@ -17,45 +17,62 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+      serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+      }
 });
 
 async function run() {
-  try {
-      const jobsCollection =  client.db('TalentFlare').collection("jobs") 
-      const categoryCollection =  client.db('TalentFlare').collection("category") 
-      
+      try {
+            const jobsCollection = client.db('TalentFlare').collection("jobs")
+            const categoryCollection = client.db('TalentFlare').collection("category")
+            const userCollection = client.db('TalentFlare').collection("users")
 
-      app.get('/jobs', async (req,res) =>{
-            const category = req.query.category
-            let query = {} 
-            if (category) {
-                  query = {category : category}
-            }
-            const jobs = await jobsCollection.find(query).toArray()
-            res.send(jobs)
-      })
-      app.get('/categories', async (req,res) =>{
-            const categories = await categoryCollection.find().toArray()
-            res.send(categories)
-      })
-      app.get('/jobs/:id' , async (req,res) => {
-            const id = req.params.id
-            const qur = {_id : new ObjectId(id)}
-            const result = await jobsCollection.findOne(qur)
-            res.send(result)
-      })
-     
-    
-   
-    console.log("SUCCESSFULLY CONNECTED TO MONGODB");
-  } finally {
-    
-  }
+            let jobs;
+
+            app.get('/jobs', async (req, res) => {
+                  const category = req.query.category
+                  let query = {}
+                  if (category) {
+                        if (category == "All category") {
+                              jobs = await jobsCollection.find().toArray()
+
+
+                        }
+
+                        else {
+                              query = { category: category }
+                              jobs = await jobsCollection.find(query).toArray()
+                        }
+
+                  }
+
+
+                  res.send(jobs)
+            })
+            app.get('/categories', async (req, res) => {
+                  const categories = await categoryCollection.find().toArray()
+                  res.send(categories)
+            })
+            app.get('/jobs/:id', async (req, res) => {
+                  const id = req.params.id
+                  const qur = { _id: new ObjectId(id) }
+                  const result = await jobsCollection.findOne(qur)
+                  res.send(result)
+            })
+            app.post('/users', async (req, res) => {
+                  const user = req.body
+                  const result = await userCollection.insertOne(user)
+                  res.send(result)
+            })
+
+
+            console.log("SUCCESSFULLY CONNECTED TO MONGODB");
+      } finally {
+
+      }
 }
 run().catch(console.dir);
 
@@ -73,10 +90,10 @@ run().catch(console.dir);
 
 
 
-app.get("/", (req,res) => {
-      res.send({status:true})
+app.get("/", (req, res) => {
+      res.send({ status: true })
 })
 
-app.listen(port ,()=>{
+app.listen(port, () => {
       console.log(`SERVER IS RUNNING ON ${port}`)
 })
